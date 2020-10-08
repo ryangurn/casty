60c254b10482348a9c1662502f8b967fa17f9928
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-08 13:39:22 -0700

    Setting up some podcast database information.

---
+++ app/Models/Podcast.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Podcast
+ * @package App\Models
+ */
+class Podcast extends Model
+{
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'image',
+        'language',
+        'categories',
+        'explicit',
+        'link',
+        'author',
+        'owner',
+        'itunes_title',
+        'itunes_type',
+        'copyright',
+        'new_feed_url',
+        'itunes_block',
+        'itunes_complete',
+        'spotify_restriction',
+        'spotify_limit',
+        'spotify_country_of_origin'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'categories' => 'array',
+        'explicit' => 'boolean',
+        'owner' => 'array',
+        'itunes_type' => 'boolean',
+        'itunes_block' => 'boolean',
+        'itunes_complete' => 'boolean',
+        'spotify_restriction' => 'array',
+    ];
+}
---
+++ database/migrations/2020_10_08_192013_create_podcasts_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // following https://help.apple.com/itc/podcasts_connect/#/itcb54353390
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->id();
+            // required things
+            $table->string('title');
+            $table->string('description');
+            $table->string('image');
+            $table->string('language');
+            $table->longText('categories');
+            $table->boolean('explicit')->default(0);
+
+            // recommended things
+            $table->text('link')->nullable();
+            $table->string('author')->nullable();
+            $table->longText('owner')->nullable();
+
+            // situational things
+            $table->string('itunes_title')->nullable();
+            $table->boolean('itunes_type')->default(0);
+            $table->string('copyright')->nullable();
+            $table->text('new_feed_url')->nullable();
+            $table->boolean('itunes_block')->default(0);
+            $table->boolean('itunes_complete')->default(0);
+
+            // spotify things
+            $table->longText('spotify_restriction')->nullable();
+            $table->integer('spotify_limit')->nullable();
+            $table->string('spotify_country_of_origin')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
