873a745cb871bb3d200fcdb1ceaea77f859fbed5
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-08 19:27:41 -0700

    Fixing header

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
+        <div class="lg:flex lg:items-center lg:justify-between">
+            <div class="flex-1 min-w-0">
+                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+                    {{ _('Dashboard') }}
+                </h2>
+            </div>
+        </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+
             </div>
         </div>
     </div>
