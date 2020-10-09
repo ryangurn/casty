a2c0f36e98bc20fd1b535fa534a46d8dfad1e251
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-08 22:24:01 -0700

    Adding podcast index page with table partial

--- resources/views/podcasts/index.blade.php
+++ resources/views/podcasts/index.blade.php
@@ -1,8 +1,35 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Podcasts') }}
-        </h2>
+        <div class="lg:flex lg:items-center lg:justify-between">
+            <div class="flex-1 min-w-0">
+                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
+                    {{ _('Podcasts') }}
+                </h2>
+            </div>
+
+
+            <div class="mt-5 flex lg:mt-0 lg:ml-4">
+                <a class="hidden sm:block ml-3 shadow-sm rounded-md" href="{{ route('podcast.create') }}">
+                    <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out">
+                    <!-- Heroicon name: plus-circle -->
+                    <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+                    Create
+                    </button>
+                </a>
+            </div>
+
+        </div>
     </x-slot>
 
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                @include('podcasts.partials.podcast-table')
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
