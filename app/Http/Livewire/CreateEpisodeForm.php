8c6e683d447c36e5a5288bf3df73381330f9e6af
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-12 11:34:51 -0700

    Adding create episode form component

--- app/Http/Livewire/CreateEpisodeForm.php
+++ app/Http/Livewire/CreateEpisodeForm.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\Country;
+use App\Models\Episode;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 
@@ -11,9 +14,91 @@ class CreateEpisodeForm extends Component
     use WithFileUploads;
 
     public $podcast;
-    public $title, $enclosure, $publishing_date, $description, $link, $explicit, $itunes_title,
-        $itunes_episode_number, $itunes_season_number, $itunes_episode_type, $spotify_order;
+    public $title, $enclosure, $publishing_date, $description, $link, $image, $explicit, $itunes_title, $itunes_type,
+        $itunes_episode_number, $itunes_season_number, $itunes_episode_type, $spotify_order, $spotify_start, $spotify_end,
+        $spotify_chapters, $spotify_keywords;
     public $spotify_countries;
+
+    protected $rules = [
+        'title' => 'required|min:3|max:255',
+        'enclosure' => 'required|file|mimes:mp3|max:205000', // 200mb (thanks spotify)
+        'publishing_date' => 'nullable|date_format:m/d/Y H:i',
+        'description' => 'nullable|min:5',
+        'link' => 'nullable|url',
+        'image' => 'nullable|file|image',
+        'explicit' => 'nullable',
+        'itunes_title' => 'nullable|min:3|max:255',
+        'itunes_episode_number' => 'nullable|numeric',
+        'itunes_season_number' => 'nullable|numeric',
+        'itunes_type' => 'nullable|in:full,trailer,bonus',
+        'spotify_countries' => 'nullable|exists:countries,id',
+        'spotify_order' => 'nullable|numeric',
+        // ensure that end is after start
+        'spotify_start' => 'nullable|date_format:m/d/y H:i',
+        'spotify_end' => 'nullable|date_format:m/d/y H:i',
+        'spotify_chapters' => 'nullable|json',
+        'spotify_keywords' => 'nullable|string'
+    ];
+
+    public function updated($property)
+    {
+        return $this->validateOnly($property);
+    }
+
+    public function createPodcast()
+    {
+        $validated = $this->validate();
+
+
+
+        $media = $this->enclosure->storeAs($this->podcast->id . '-media', time() . '.'. $this->enclosure->extension(), 'public');
+        if (isset($this->image) && $this->image != null)
+            $photo = $this->image->storeAs($this->podcast->id . '-media', time().'.'.$this->image->extension(), 'public');
+
+        $item = [];
+        $item['podcast_id'] = $this->podcast->id;
+        $item['title'] = $validated['title'];
+        $item['enclosure'] = ['file' => $media, 'tag' =>
+                                    '<enclosure
+                                     url="'.Storage::url($media).'" 
+                                     length="'.$this->enclosure->getSize().'"
+                                     type="audio/mpeg
+                                    />'
+                                ];
+        $item['guid'] = Str::uuid();
+        $item['publishing_date'] = $validated['publishing_date'];
+        $item['description'] = $validated['description'];
+        $item['link'] = $validated['link'];
+        if (isset($this->image) && $this->image != null)
+            $item['image'] = $validated['image'];
+        $item['explicit'] = ($validated['explicit'] == null) ? false : true;
+        $item['itunes_title'] = $validated['itunes_title'];
+        $item['itunes_episode_number'] = $validated['itunes_episode_number'];
+        $item['itunes_season_number'] = $validated['itunes_season_number'];
+        if (!isset($validated['itunes_episode_type']) || $validated['itunes_episode_type'] == null || $validated['itunes_episode_type'] == "full")
+        {
+            $item['itunes_episode_type'] = 0;
+        }
+        elseif ($validated['itunes_episode_type'] == null && $validated['itunes_episode_type'] == "trailer")
+        {
+            $item['itunes_episode_type'] = 1;
+        }
+        elseif ($validated['itunes_episode_type'] == null && $validated['itunes_episode_type'] == "bonus")
+        {
+            $item['itunes_episode_type'] = 2;
+        }
+        $item['spotify_restriction'] = $validated['spotify_countries'];
+        $item['order'] = $validated['spotify_order'];
+        $item['spotify_start'] = $validated['spotify_start'];
+        $item['spotify_end'] = $validated['spotify_end'];
+        $item['spotify_chapters'] = $validated['spotify_chapters'];
+        $item['spotify_keywords'] = explode(",", $validated['spotify_keywords']);
+
+        Episode::create($item);
+
+        return redirect()->route('episode.index', $this->podcast->id);
+
+    }
 
     public function render()
     {
--- resources/views/episodes/partials/create-episode-form.blade.php
+++ resources/views/episodes/partials/create-episode-form.blade.php
@@ -11,18 +11,23 @@
     <x-slot name="form">
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="title" value="{{ __('Episode Title') }}" />
-            <x-jet-input id="title" type="text" class="mt-1 block w-full" wire:model.defer="title" autofocus />
+            <x-jet-input type="text" class="mt-1 block w-full" wire:model.defer="title" autofocus />
             <x-jet-input-error for="title" class="mt-2" />
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="enclosure" value="{{ __('Episode Audio File') }}" />
+            <x-jet-label for="enclosure" value="{{ __('Episode Audio File (only mp3 due to spotify restrictions)') }}" />
             <input type="file" wire:model="enclosure">
             <x-jet-input-error for="enclosure" class="mt-2" />
+
+{{--            <div class="mt-8">--}}
+{{--            --}}{{-- this is where the audio component will go later --}}
+{{--            </div>--}}
+
         </div>
 
         <div class="col-span-6 sm:col-span-4">
-            <x-jet-label for="publishing_date" value="{{ __('Publishing Date (optional, mm/dd/yyyy)') }}" />
+            <x-jet-label for="publishing_date" value="{{ __('Publishing Date (optional, ex: 01/20/2015 13:00)') }}" />
             <x-jet-input id="publishing_date" type="text" class="mt-1 block w-full" wire:model.defer="publishing_date" autofocus />
             <x-jet-input-error for="publishing_date" class="mt-2" />
         </div>
@@ -37,6 +42,19 @@
             <x-jet-label for="link" value="{{ __('Episode Link (optional)') }}" />
             <x-jet-input name="link" id="link" type="text" class="mt-1 block w-full" wire:model="link" autofocus />
             <x-jet-input-error for="link" class="mt-2" />
+        </div>
+
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="image" value="{{ __('Episode Image (optional)') }}" />
+            <input type="file" wire:model="image">
+            <x-jet-input-error for="image" class="mt-2" />
+
+            <div>
+                @if ($image)
+                    <img class="w-32 h-32 m-4 rounded-md" src="{{ $image->temporaryUrl() }}">
+                @endif
+            </div>
+
         </div>
 
         <div class="col-span-6 sm:col-span-6">
@@ -86,7 +104,7 @@
                     <option value="bonus">Bonus</option>
                 </select>
             </div>
-            <x-jet-input-error for="itunes_type" class="mt-2" />
+            <x-jet-input-error for="itunes_episode_type" class="mt-2" />
         </div>
 
         @if(isset($countries) && !$countries->isEmpty())
@@ -110,157 +128,29 @@
             <x-jet-input-error for="spotify_order" class="mt-2" />
         </div>
 
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="image" value="{{ __('Podcast Image') }}" />--}}
-{{--            <input type="file" wire:model="image">--}}
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="spotify_start" value="{{ __('Spotify Viewable Start Time (optional, ex: 01/20/2015 13:00)') }}" />
+            <x-jet-input id="spotify_start" type="text" class="mt-1 block w-full" wire:model.defer="spotify_start" autofocus />
+            <x-jet-input-error for="spotify_start" class="mt-2" />
+        </div>
 
-{{--            @if ($image)--}}
-{{--                <img class="w-32 h-32 m-4 rounded-md" src="{{ $image->temporaryUrl() }}">--}}
-{{--            @endif--}}
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="spotify_end" value="{{ __('Spotify Viewable End Time (optional, ex: 01/20/2015 13:00)') }}" />
+            <x-jet-input id="spotify_end" type="text" class="mt-1 block w-full" wire:model.defer="spotify_end" autofocus />
+            <x-jet-input-error for="spotify_end" class="mt-2" />
+        </div>
 
-{{--            <x-jet-input-error for="image" class="mt-2" />--}}
-{{--        </div>--}}
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="spotify_chapters" value="{{ __('Spotify Chapters (optional and in json form)') }} [{'start':'0', 'title':'Opening'}, ...]" />
+            <x-textarea name="spotify_chapters" id="spotify_chapters" class="mt-1 block w-full" wire:model="spotify_chapters" autofocus />
+            <x-jet-input-error for="spotify_chapters" class="mt-2" />
+        </div>
 
-{{--        @if(isset($languages) && !$languages->isEmpty())--}}
-{{--            <div class="col-span-6 sm:col-span-4">--}}
-{{--                <x-jet-label for="language" value="{{ __('Language') }}" />--}}
-{{--                <div class="relative inline-flex">--}}
-{{--                    <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>--}}
-{{--                    <select class="w-full border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none form-input" wire:model="language">--}}
-{{--                        <option disabled>Podcast Language</option>--}}
-{{--                        @foreach($languages as $language)--}}
-{{--                            <option value="{{ $language->id }}">{{ $language->name }}</option>--}}
-{{--                        @endforeach--}}
-{{--                    </select>--}}
-{{--                </div>--}}
-{{--                <x-jet-input-error for="language" class="mt-2" />--}}
-{{--            </div>--}}
-{{--        @endif--}}
-
-{{--        @if(isset($categories) && !$categories->isEmpty())--}}
-{{--            <div class="col-span-6 sm:col-span-6">--}}
-{{--                <x-jet-label for="categories[]" value="{{ __('Categories') }}" />--}}
-{{--                <div class="relative inline-flex">--}}
-{{--                    <select name="categories[]" class="form-multiselect block w-full" multiple id="categories[]" wire:model="podcast_categories">--}}
-{{--                        <option disabled>Podcast Categories</option>--}}
-{{--                        @foreach($categories as $category)--}}
-{{--                            <option value="{{ $category->id }}">@if($category->category != null){{ '[' . $category->category->name . '] - ' }}@endif{{ $category->name }}</option>--}}
-{{--                        @endforeach--}}
-{{--                    </select>--}}
-{{--                </div>--}}
-{{--                <x-jet-input-error for="podcast_categories" class="mt-2" />--}}
-{{--            </div>--}}
-{{--        @endif--}}
-
-{{--        <div class="col-span-6 sm:col-span-6">--}}
-{{--            <x-jet-label for="explicit" value="{{ __('Explicit Content') }}" />--}}
-{{--            <label class="flex justify-start items-start">--}}
-{{--                <div class="bg-white border-2 rounded border-gray-400 w-6 h-6 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-blue-500">--}}
-{{--                    <input name="explicit" type="checkbox" class="opacity-0 absolute" wire:model="explicit">--}}
-{{--                    <svg class="fill-current hidden w-4 h-4 text-green-500 pointer-events-none" viewBox="0 0 20 20"><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>--}}
-{{--                </div>--}}
-{{--                <div class="select-none">Explicit Content</div>--}}
-{{--            </label>--}}
-
-{{--            <style>--}}
-{{--                input:checked + svg {--}}
-{{--                    display: block;--}}
-{{--                }--}}
-{{--            </style>--}}
-{{--            <x-jet-input-error for="explicit" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="link" value="{{ __('Podcast Website Link (optional)') }}" />--}}
-{{--            <x-jet-input name="link" id="link" type="text" class="mt-1 block w-full" wire:model="link" autofocus />--}}
-{{--            <x-jet-input-error for="link" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="author.name" value="{{ __('Author Name (optional)') }}" />--}}
-{{--            <x-jet-input name="author.name" id="author.name" type="text" class="mt-1 block w-full" wire:model="author.name" autofocus />--}}
-{{--            <x-jet-input-error for="author.name" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="author.email" value="{{ __('Author Email (optional)') }}" />--}}
-{{--            <x-jet-input name="author.email" id="author.email" type="text" class="mt-1 block w-full" wire:model="author.email" autofocus />--}}
-{{--            <x-jet-input-error for="author.email" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="owner.name" value="{{ __('Owner Name (optional)') }}" />--}}
-{{--            <x-jet-input name="owner.name" id="owner.name" type="text" class="mt-1 block w-full" wire:model="owner.name" autofocus />--}}
-{{--            <x-jet-input-error for="owner.name" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="owner.email" value="{{ __('Owner Email (optional)') }}" />--}}
-{{--            <x-jet-input name="owner.email" id="owner.email" type="text" class="mt-1 block w-full" wire:model="owner.email" autofocus />--}}
-{{--            <x-jet-input-error for="owner.email" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="itunes_title" value="{{ __('iTunes Title (optional)') }}" />--}}
-{{--            <x-jet-input name="itunes_title" id="itunes_title" type="text" class="mt-1 block w-full" wire:model="itunes_title" autofocus />--}}
-{{--            <x-jet-input-error for="itunes_title" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="itunes_type" value="{{ __('iTunes Type (defaults to episodic)') }}" />--}}
-{{--            <div class="relative inline-flex">--}}
-{{--                <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>--}}
-{{--                <select name="itunes_type" class="w-full border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none form-input" wire:model="itunes_type">--}}
-{{--                    <option disabled>iTunes Type</option>--}}
-{{--                    <option value="episodic">Episodic</option>--}}
-{{--                    <option value="serial">Serial</option>--}}
-{{--                </select>--}}
-{{--            </div>--}}
-{{--            <x-jet-input-error for="itunes_type" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="copyright" value="{{ __('Copyright (optional)') }}" />--}}
-{{--            <x-textarea name="copyright" id="copyright" class="mt-1 block w-full" wire:model="copyright" autofocus />--}}
-{{--            <x-jet-input-error for="copyright" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        @if(isset($countries) && !$countries->isEmpty())--}}
-{{--            <div class="col-span-6 sm:col-span-6">--}}
-{{--                <x-jet-label for="countries[]" value="{{ __('Spotify Country Whitelist (optional)') }}" />--}}
-{{--                <div class="relative inline-flex">--}}
-{{--                    <select class="form-multiselect block w-full" multiple id="my-multiselect" wire:model="spotify_countries">--}}
-{{--                        <option disabled>Countries</option>--}}
-{{--                        @foreach($countries as $country)--}}
-{{--                            <option value="{{ $country->id }}">{{ $country->name }}</option>--}}
-{{--                        @endforeach--}}
-{{--                    </select>--}}
-{{--                </div>--}}
-{{--                <x-jet-input-error for="spotify_countries" class="mt-2" />--}}
-{{--            </div>--}}
-{{--        @endif--}}
-
-{{--        <div class="col-span-6 sm:col-span-4">--}}
-{{--            <x-jet-label for="spotify_limit" value="{{ __('Spotify Display Limit (optional)') }}" />--}}
-{{--            <x-jet-input name="spotify_limit" id="spotify_limit" type="text" class="mt-1 block w-full" wire:model="spotify_limit" autofocus />--}}
-{{--            <x-jet-input-error for="spotify_limit" class="mt-2" />--}}
-{{--        </div>--}}
-
-{{--        @if(isset($countries) && !$countries->isEmpty())--}}
-{{--            <div class="col-span-6 sm:col-span-6">--}}
-{{--                <x-jet-label for="spotify_origin" value="{{ __('Spotify Country of Origin (optional)') }}" />--}}
-{{--                <div class="relative inline-flex">--}}
-{{--                    <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>--}}
-{{--                    <select class="w-full border border-gray-300 text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none form-input" wire:model="spotify_origin">--}}
-{{--                        <option disabled>Origin Country</option>--}}
-{{--                        @foreach($countries as $country)--}}
-{{--                            <option value="{{ $country->id }}">{{ $country->name }}</option>--}}
-{{--                        @endforeach--}}
-{{--                    </select>--}}
-{{--                </div>--}}
-{{--                <x-jet-input-error for="spotify_origin" class="mt-2" />--}}
-{{--            </div>--}}
-{{--        @endif--}}
+        <div class="col-span-6 sm:col-span-4">
+            <x-jet-label for="spotify_keywords" value="{{ __('Spotify Keywords (optional, comma separated)') }}" />
+            <x-jet-input id="spotify_keywords" type="text" class="mt-1 block w-full" wire:model.defer="spotify_keywords" autofocus />
+            <x-jet-input-error for="spotify_keywords" class="mt-2" />
+        </div>
 
     </x-slot>
 
@@ -270,3 +160,7 @@
         </x-jet-button>
     </x-slot>
 </x-jet-form-section>
+
+@section('styles')
+{{--    <link rel="stylesheet" href="{{ asset('css/tailwind.min.css') }}" />--}}
+@endsection
