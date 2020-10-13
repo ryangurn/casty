7f5440c5d58405f44eda1e8c39394c1e039ea2ce
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-12 18:58:47 -0700

    making episodes table dynamic

--- resources/views/episodes/partials/episode-table.blade.php
+++ resources/views/episodes/partials/episode-table.blade.php
@@ -2,13 +2,13 @@
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table class="min-w-full divide-y divide-gray-200">
+                <table class="min-w-ful divide-gray-200 table-auto">
                     <thead>
                     <tr>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Podcast</th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Categories</th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Language</th>
-                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Info</th>
+                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Episode</th>
+                        <th class="py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">iTunes</th>
+                        <th class="py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Spotify</th>
+{{--                        <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Info</th>--}}
                         <th class="px-6 py-3 bg-gray-50"></th>
                     </tr>
                     </thead>
@@ -17,70 +17,90 @@
                             @foreach($episodes as $episode)
                                 <tr>
                                     <td class="px-6 py-4 whitespace-no-wrap">
-                                        <a href="#show" class="flex items-center">
+                                        <a href="{{ route('episode.show', [$episode->podcast->id, $episode->id]) }}" class="flex items-center">
+                                            @if (isset($episode->image) && $episode->image != null)
                                             <div class="flex-shrink-0 h-10 w-10">
-                                                <img class="h-10 w-10 rounded-md" />
+                                                <img class="h-10 w-10 rounded-md" src="{{ Storage::url($episode->image) }}" alt="">
+                                            </div>
+                                            @endif
+                                            <div class="ml-4">
+                                                <div class="text-sm leading-5 font-medium text-gray-900">
+                                                    {{ $episode->title }}
+                                                </div>
+                                                @if(isset($episode->description) && $episode->description != null)
+                                                <div class="text-sm leading-5 text-gray-500">
+                                                    @if(strlen($episode->description) > 50)
+                                                        {{ substr($episode->description, 0, 50) }}...
+                                                    @else
+                                                        {{ $episode->description }}
+                                                    @endif
+                                                </div>
+                                                @endif
                                             </div>
                                         </a>
+                                    </td>
+                                    <td class="px=6 py-4 whitespace-no-wrap">
+                                        <div class="flex flex-wrap">
+                                        @if(isset($episode->itunes_title) && $episode->itunes_title != null)
+                                            <div class="rounded-full px-4 mr-2 mt-2 bg-blue-600 text-white p-2 rounded leading-none flex items-center">
+                                                Title: {{ $episode->itunes_title }}
+                                            </div>
+                                        @endif
+                                        @if(isset($episode->itunes_episode_number) && $episode->itunes_episode_number != null)
+                                            <div class="rounded-full px-4 mr-2 mt-2 bg-red-600 text-white p-2 rounded leading-none flex items-center">
+                                                Episode #: {{ $episode->itunes_episode_number }}
+                                            </div>
+                                        @endif
+                                        @if(isset($episode->itunes_episode_type) && $episode->itunes_episode_type != null)
+                                            <div class="rounded-full px-4 mr-2 mt-2 bg-orange-600 text-white p-2 rounded leading-none flex items-center">
+                                                Type: {{ $episode->itunes_episode_type }}
+                                            </div>
+                                        @endif
+                                        </div>
+                                    </td>
+                                    <td class="px=6 py-4 whitespace-no-wrap">
+                                        <div class="flex flex-wrap">
+                                        @if(!empty($podcast->spotify_restriction))
+                                            @foreach($podcast->spotify_restriction as $sr)
+                                                <span class="px-2 mt-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-blue-500 text-black-800">
+                                                  {{ \App\Models\Country::where('id', '=', $sr)->first()->name }}
+                                                </span>
+                                            @endforeach
+                                        @endif
+                                        @if(isset($episode->order) && $episode->order != null)
+                                            <span class="rounded-full px-4 mr-2 mt-2 bg-red-600 text-white p-2 rounded  leading-none flex items-center">
+                                                Order: {{ $episode->order }}
+                                            </span>
+                                        @endif
+                                        @if(isset($episode->spotify_start, $episode->spotify_end) && $episode->spotify_start != null && $episode->spotify_end != null)
+                                            <span class="rounded-full px-4 mr-2 mt-2 bg-orange-600 text-white p-2 rounded  leading-none flex items-center">
+                                                Starting: {{ date("m/d/Y H:i", $episode->spotify_start) }}
+                                            </span>
+                                            <span class="rounded-full px-4 mr-2 mt-2 bg-orange-600 text-white p-2 rounded  leading-none flex items-center">Ending: {{ date("m/d/Y H:i", $episode->spotify_end) }}
+                                            </span>
+                                        @endif
+                                        @if(isset($episode->spotify_chapters) && $episode->spotify_chapters != null)
+                                            <span class="rounded-full px-4 mr-2 mt-2 bg-green-600 text-white p-2 rounded  leading-none flex items-center">
+                                                Chapters: {{ count(json_decode($episode->spotify_chapters)) }}
+                                            </span>
+                                        @endif
+                                        @if(isset($episode->spotify_keywords) && $episode->spotify_keywords != null && $episode->spotify_keywords[0] != "")
+                                            <span class="rounded-full px-4 mr-2 mt-2 bg-purple-600 text-white p-2 rounded  leading-none flex items-center">
+                                                Keywords: {{ implode(", ", $episode->spotify_keywords) }}
+                                            </span>
+                                        @endif
+                                        </div>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
+                                        <a href="{{ route('episode.edit', [$episode->podcast->id, $episode->id]) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
                                     </td>
                                 </tr>
                             @endforeach
                         @else
                             <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap" colspan="5">{{ _('No episodes setup yet. :(') }}</td>
+                                <td class="px-6 py-4 whitespace-no-wrap" colspan="5">{{ __('No episodes setup yet. :(') }}</td>
                             </tr>
                         @endif
-{{--                    @if (isset($podcasts) && !$podcasts->isEmpty())--}}
-{{--                        @foreach($podcasts as $podcast)--}}
-{{--                            <tr>--}}
-{{--                                <td class="px-6 py-4 whitespace-no-wrap">--}}
-{{--                                    <a href="{{ route('podcast.show', $podcast->id) }}" class="flex items-center">--}}
-{{--                                        <div class="flex-shrink-0 h-10 w-10">--}}
-{{--                                            <img class="h-10 w-10 rounded-md" src="{{ Storage::url($podcast->image) }}" alt="">--}}
-{{--                                        </div>--}}
-{{--                                        <div class="ml-4">--}}
-{{--                                            <div class="text-sm leading-5 font-medium text-gray-900">--}}
-{{--                                                {{ $podcast->title }}--}}
-{{--                                            </div>--}}
-{{--                                            <div class="text-sm leading-5 text-gray-500">--}}
-{{--                                                @if(strlen($podcast->description) > 50)--}}
-{{--                                                    {{ substr($podcast->description, 0, 50) }}...--}}
-{{--                                                @else--}}
-{{--                                                    {{ $podcast->description }}--}}
-{{--                                                @endif--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-{{--                                    </a>--}}
-{{--                                </td>--}}
-{{--                                <td class="px-6 py-4 whitespace-no-wrap">--}}
-{{--                                    @if(count($podcast->categories) != 0)--}}
-{{--                                        @foreach($podcast->categories as $cat)--}}
-{{--                                            <div class="text-sm leading-5 text-gray-900">{{ \App\Models\PodcastCategory::where('id', '=', $cat)->first()->name }}</div>--}}
-{{--                                        @endforeach--}}
-{{--                                    @else--}}
-{{--                                        <div class="text-sm leading-5 text-red-900">No Categories Yet.</div>--}}
-{{--                                    @endif--}}
-{{--                                </td>--}}
-{{--                                <td class="px-6 py-4 whitespace-no-wrap">--}}
-{{--                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">--}}
-{{--                              {{ $podcast->getLanguage->name }}--}}
-{{--                            </span>--}}
-{{--                                </td>--}}
-{{--                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">--}}
-{{--                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full @if ($podcast->itunes_type == "Episodic"){{ 'bg-blue-100' }}@elseif($podcast->itunes_type == "Serial"){{ 'bg-yellow-100' }}@endif text-black-800">--}}
-{{--                              {{ $podcast->itunes_type }}--}}
-{{--                            </span>--}}
-{{--                                </td>--}}
-{{--                                <td class="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">--}}
-{{--                                    <a href="{{ route('podcast.edit', $podcast->id) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>--}}
-{{--                                </td>--}}
-{{--                            </tr>--}}
-{{--                        @endforeach--}}
-{{--                    @else--}}
-{{--                        <tr>--}}
-{{--                            <td class="px-6 py-4 whitespace-no-wrap" colspan="5">{{ _('No podcasts setup yet. :(') }}</td>--}}
-{{--                        </tr>--}}
-{{--                    @endif--}}
                     </tbody>
                 </table>
             </div>
