876f1bb98e7789e89ba64f50b5f4645c826275c8
Ryan Gurnick <ryangurnick@gmail.com>
2020-10-26 18:53:11 -0700

    Working on making audio dynamic

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episode;
 use App\Models\Podcast;
 use App\Models\PodcastCategory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class AssetController extends Controller
 {
+    /**
+     * @param Podcast $podcast
+     * @return Response
+     */
     public function iTunes(Podcast $podcast)
     {
         $pcs = $podcast->categories;
@@ -30,6 +37,10 @@ class AssetController extends Controller
             ->header('Content-Type', 'text/xml');
     }
 
+    /**
+     * @param Podcast $podcast
+     * @return Response
+     */
     public function Spotify(Podcast $podcast)
     {
         $pcs = $podcast->categories;
@@ -50,5 +61,23 @@ class AssetController extends Controller
         return response()
             ->view('assets.spotify', compact('podcast', 'categories'))
             ->header('Content-Type', 'text/xml');
+    }
+
+    /**
+     *
+     */
+    public function audio($guid)
+    {
+        $episode = Episode::where('guid', '=', $guid)->first();
+
+        $file = $episode->enclosure['file'];
+        $f = Storage::disk('public')->get($file);
+
+        return response($f)
+            ->header('Content-Length', $episode->enclosure['length'])
+            ->header('Content-Type', 'audio/mpeg');
+//        return (new Response($file, 200))
+//            ->header('Content-Type', 'audio/mpeg');
+
     }
 }
